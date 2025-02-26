72727577a46809980333ac9e65f875957842c40d
gandia garcia-morales laura <imposiblenormalito@gmail.com>
2025-02-26 11:27:56 +0000

    created enum values

--- patient.java
+++ patient.java
@@ -1,3 +1,41 @@
 class patient {
+    private enum AirOrOxygen{
+        AIR(0),
+        OXYGEN(2);
 
+        // Constructor
+        private final int value;
+        AirOrOxygen(int value){
+            this.value = value;
+        }
+
+        // Getter
+        public int getValue(){
+            return value;
+        }
+    }
+
+    private enum Conciousness{
+        ALERT(0), CVPU(1);
+
+        // Constructor
+        private final int value;
+        Conciousness(int value) {
+            this.value = value;
+        }
+        // Getter
+        public int getValue(){
+            return value;
+        }
+
+        // Method to return whether the patient is conscious or alert
+        public static Conciousness getConciousness(int value){
+            // if value is 0, then ALERT is returned, else CVPU is returned
+            if (value == 0){
+                return ALERT;
+            }else {
+                return CVPU;
+            }
+        }
+    }
 }
