bfd452a02ba014a76972de7c9e2aa89cf277213a
gandia garcia-morales laura <imposiblenormalito@gmail.com>
2025-02-26 11:55:44 +0000

    tested in driver class, created constructor/getters for variables and remove the private from enums

--- Driver.java
+++ Driver.java
@@ -1,4 +1,16 @@
 public class Driver {
     public static void main(String[] args) {
+        Patient.AirOrOxygen airOrOxygen = Patient.AirOrOxygen.OXYGEN;
+        Patient.Consciousness consciousness = Patient.Consciousness.ALERT;
+
+        // Create a Patient object
+        Patient patient = new Patient(airOrOxygen, consciousness, 20, 95, 37.5F);
+
+        // Print the details of the patient
+        System.out.println("Air or Oxygen: " + patient.getAirOrOxygen());
+        System.out.println("Consciousness: " + patient.getConciousness());
+        System.out.println("Respiration Range: " + patient.getRespirationRange());
+        System.out.println("SpO2: " + patient.getSp02());
+        System.out.println("Temperature: " + patient.getTemperature());
     }
 }
--- Patient.java
+++ Patient.java
@@ -1,46 +1,81 @@
 public class Patient {
-    private enum AirOrOxygen{
+    enum AirOrOxygen {
         AIR(0),
         OXYGEN(2);
 
         // Constructor
         private final int value;
-        AirOrOxygen(int value){
+
+        AirOrOxygen(int value) {
             this.value = value;
         }
 
         // Getter
-        public int getValue(){
+        public int getValue() {
             return value;
         }
     }
 
-    private enum Conciousness{
+    enum Consciousness {
         ALERT(0), CVPU(1);
 
         // Constructor
         private final int value;
-        Conciousness(int value) {
+
+        Consciousness(int value) {
             this.value = value;
         }
+
         // Getter
-        public int getValue(){
+        public int getValue() {
             return value;
         }
 
         // Method to return whether the patient is conscious or alert
-        public static Conciousness getConciousness(int value){
+        public static Consciousness getConciousness(int value) {
             // if value is 0, then ALERT is returned, else CVPU is returned
-            if (value == 0){
+            if (value == 0) {
                 return ALERT;
-            }else {
+            } else {
                 return CVPU;
             }
         }
     }
 
-    private int RespirationRange;
-    private int Sp02;
+    // Attributes
+    private AirOrOxygen airOrOxygen;
+    private Consciousness conciousness;
+    private int respirationRange;
+    private int sp02;
     private float temperature;
 
+    // Constructor
+    public Patient(AirOrOxygen airOrOxygen, Consciousness conciousness, int respirationRange, int sp02, float temperature) {
+        this.airOrOxygen = airOrOxygen;
+        this.conciousness = conciousness;
+        this.respirationRange = respirationRange;
+        this.sp02 = sp02;
+        this.temperature = temperature;
+
+    }
+    // Getters
+    public AirOrOxygen getAirOrOxygen() {
+        return airOrOxygen;
+    }
+
+    public Consciousness getConciousness() {
+        return conciousness;
+    }
+
+    public int getRespirationRange() {
+        return respirationRange;
+    }
+
+    public int getSp02() {
+        return sp02;
+    }
+
+    public float getTemperature() {
+        return temperature;
+    }
 }
